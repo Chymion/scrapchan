6f31febac20f8d3add5e85cff0106403f7a06eb9
Loryon <shengeki575@gmail.com>
2022-01-15 03:46:05 +0100

    Added cloves and minor improvements

--- scrapchan.py
+++ scrapchan.py
@@ -2,7 +2,7 @@ import os
 import re
 
 os.system('clear')
-print("=============\n SCRAPCHAN\n=============\n\n")
+print("=============\n🍀SCRAPCHAN🍀\n=============\n")
 
 url = input("Enter the thread URL: ")
 print(url)
@@ -13,13 +13,13 @@ print("1- Video (mp4,webm)\n2- Gif\n3- Image (jpg,png)\n4- All\n\n")
 choice = input("Format: ")
 choiceint = int(choice)
 if (choiceint == 1):
-    os.system('curl ' + url + ' 2>&1 | grep -o -E \'href=\"//([^\"#]+)webm\\|mp4\"\' > links.temp');
+    os.system('curl ' + url + ' 2>&1 | grep -Eo \'href="[^\"]+"\' | grep \'webm\|mp4\' > links.temp');
 if (choiceint == 2):
-    os.system('curl ' + url + ' 2>&1 | grep -o -E \'href=\"//([^\"#]+)gif\"\' > links.temp');
+    os.system('curl ' + url + ' 2>&1 | grep -Eo \'href="[^\"]+"\' | grep \'gif\' > links.temp');
 if (choiceint == 3):
-    os.system('curl ' + url + ' 2>&1 | grep -o -E \'href=\"//([^\"#]+)jpg\\|png\"\' > links.temp');
+    os.system('curl ' + url + ' 2>&1 | grep -Eo \'href="[^\"]+"\' | grep \'jpg\|png\' > links.temp');
 if (choiceint == 4):
-    os.system('curl ' + url + ' 2>&1 | grep -o -E \'href=\"//([^\"#]+)jpg\"\' > links.temp');
+    os.system('curl ' + url + ' 2>&1 | grep -Eo \'href="[^\"]+"\' | grep \'jpg\|png\|webm\|mp4\|gif\' > links.temp');
 
 for line in open("links.temp"):
     line = re.sub('"|^\w+="', "https:", line)
@@ -27,8 +27,3 @@ for line in open("links.temp"):
     print(line)
     os.system('cd output/ && wget -N ' + line)
 os.remove("links.temp")
-
-
-
-
-
